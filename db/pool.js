d650c418e7484a47a0bce7da90e5216926f13c75
deepend15 <170750487+deepend15@users.noreply.github.com>
2026-02-25 23:34:26 -0500

    Edit db/pool.js
    Define connection string for the pool, rather than each individual
    property. Allows easy storage of the connection string, which will be
    used to populate the db.

--- db/pool.js
+++ db/pool.js
@@ -1,9 +1,5 @@
 import { Pool } from "pg";
 
 export default new Pool({
-  host: process.env.PGHOST,
-  user: process.env.PGUSER,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT,
+  connectionString: `postgresql://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}:${process.env.PGPORT}/${process.env.PGDATABASE}`,
 });
